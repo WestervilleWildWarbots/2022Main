a2981c8c0dab466b67a76b92d79fd1076c42f6af
mcquickster <lukescoop2@gmail.com>
2022-01-30 07:25:24 -0500

    lukes testing

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,12 +1,13 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Drive{
     private CANSparkMax testMotor;
     
     public Drive(){
-    testMotor = new CANSparkMax(11);
+    testMotor = new CANSparkMax(11, MotorType.kBrushless);
     }
     
     public void go(double speed){
