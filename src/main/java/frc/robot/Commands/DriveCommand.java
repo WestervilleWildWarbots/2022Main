c17c70c951d5663a6016b600f3be019647e91f84
311993 <squirrellord19@gmail.com>
2022-01-31 19:32:16 -0500

    commit

--- src/main/java/frc/robot/Commands/DriveCommand.java
+++ src/main/java/frc/robot/Commands/DriveCommand.java
@@ -1,5 +1,4 @@
 package frc.robot.Commands;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.DriveSubsystem;
@@ -21,8 +20,32 @@ public class DriveCommand extends CommandBase{
 
     @Override
     public void execute(){
+      final double speedscale = 0.7;
+    final double deadzoneY = 0.2;
+    final double deadzoneZ = 0.35;     
 
+    double y = driveStick.getY();
+    double z = driveStick.getZ();
+
+    //Deadzone Code
+    if (Math.abs(y) <= deadzoneY) {
+      y = 0;
     }
+
+    if (Math.abs(z) <= deadzoneZ) {
+      z = 0;
+    }
+   
+    z*=.4*Math.abs(z);
+
+    double leftPower = speedscale*-(z-y);
+    double rightPower = speedscale*(z+y);
+
+    driveSubsystem.tankDrive(leftPower, rightPower);
+
+  }
+
+    
     
     @Override
     public boolean isFinished() {
