37cc16758f516458782eaf2eda73adea211cf195
311993 <squirrellord19@gmail.com>
2022-01-31 19:58:29 -0500

    Possibly functional Shooter command and Subsystem. Will fix later

--- src/main/java/frc/robot/Commands/ShootCommand.java
+++ src/main/java/frc/robot/Commands/ShootCommand.java
@@ -1,11 +1,19 @@
 package frc.robot.Commands;
 
+import java.text.BreakIterator;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.ShootSubsystem;
+import frc.robot.Robot;
+
+
+
 
 public class ShootCommand extends CommandBase{
 
     private ShootSubsystem shootSubsystem;
+    public int Preset = 1;
+    public boolean FireAtWill = false;
 
     public ShootCommand(ShootSubsystem shootSubsystem){
         this.shootSubsystem = shootSubsystem;
@@ -19,7 +27,26 @@ public class ShootCommand extends CommandBase{
     @Override
     public void execute(){
         //MUST SMOOTHLY RAMP between current speed and desired speed
+        //First, detect Preset(Just for testing purposes for desired speed)
+        //Second, Once Preset is checked, get desired maximum speed(This is the Preset)
+        //Third, Once desired speed reached, Hold for ~1.5 seconds(Testing, make ~.25 - ~.5 seconds for actual)
+        //Fourth, Begin Ramp Down procedure to initial speed of 0
+        if (FireAtWill == true) {
+            switch(Preset) {
+                case 1:shootSubsystem.BeginRamp(0.25);
+                break;
+                case 2:shootSubsystem.BeginRamp(0.55);
+                break;
+                case 3:shootSubsystem.BeginRamp(0.75);
+                break;
+            }
+            FireAtWill = false;
+        }
+       
+
+
         //TODO: sense color and spit out ball if not our team color
+
     }
     
     @Override
--- src/main/java/frc/robot/Subsystems/ShootSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ShootSubsystem.java
@@ -1,9 +1,20 @@
 package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Commands.ShootCommand;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import java.lang.AutoCloseable;
+import java.sql.Time;
 
 public class ShootSubsystem extends SubsystemBase{
-    
+    public CANSparkMax FlywheelMotor;
+    private double HalfSpeed;
+    private double QuarterSpeed;
+    private double ThreeQuarterSpeed;
+    private ShootCommand shootCommand;
     /*
     Flywheel Motor / Redline? / SparkMax 30 
     Turret Rotation Motor / Redline? / SparkMax 34 
@@ -12,11 +23,44 @@ public class ShootSubsystem extends SubsystemBase{
     Color Sensor 52
     Beam Break Sensor 53
      */
-
-    public ShootSubsystem(){
-
-    }
     
+    public ShootSubsystem(ShootCommand shootCommand){
+        FlywheelMotor = new CANSparkMotor(14 MotorType.kBrushless);
+        this.shootCommand = shootCommand;
+    }
+
+    public void BeginRamp(double DesiredSpeed) { //Begin ramping the motor to the Desired speed as specified in ShootCommand
+       QuarterSpeed = DesiredSpeed*0.25;
+       HalfSpeed = DesiredSpeed*0.5;
+       ThreeQuarterSpeed = DesiredSpeed*0.75;
+        if(shootCommand.FireAtWill == true){
+            double Time = 0;
+            if(Time == 0){
+                while(Time<=0.25){
+                    FlywheelMotor.set(QuarterSpeed);
+                    Time+=0.01
+                }
+            }
+            if(Time == 0.26){
+                while(Time<=0.50){
+                    FlywheelMotor.set(HalfSpeed);
+                    Time+=0.01
+                }
+            }
+            if(Time == 0.51){
+                while(Time<=0.75){
+                    FlywheelMotor.set(ThreeQuarterSpeed);
+                    Time+=0.01
+                }
+            }
+            if(Time == 0.76){
+                while(Time<=1){
+                    FlywheelMotor.set(DesiredSpeed);
+                    Time+=0.01
+                }
+            }
+       }
+    }
     /*
     Methods:
         Void:
