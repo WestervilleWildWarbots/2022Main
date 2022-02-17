e4be6948eac1082f009f7885df8f752db247f941
311993 <squirrellord19@gmail.com>
2022-02-16 19:47:00 -0500

    basic spin intake method

--- src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -1,23 +1,33 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase{
     
     /*
-    Intake Motor / Redline / SparkMax 40 
+    //Intake Motor / Redline / SparkMax 40 
     Latch servo L 41 
     Latch servo R 42 
     */
 
-    public IntakeSubsystem(){
+    CANSparkMax intakeMotor;
 
+    public IntakeSubsystem(){
+        intakeMotor = new CANSparkMax(40,MotorType.kBrushless);
+    }
+
+    public void spin(double speed){
+        intakeMotor.set(speed);
     }
 
     /*
     Methods:
         Void:
-            setSpeed - set intake motor to given speed
+            /setSpeed - set intake motor to given speed
             release - set latch servos to released state
             latch - set latch servos to latched state
         Return:
